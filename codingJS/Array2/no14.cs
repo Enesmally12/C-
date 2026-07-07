a2779dc4a292e9ca05e8f79cec68862b5f47e5f6
ENE-DEDE PRECIOUS DEINMA <enededeprecious1@gmail.com>
2026-07-07 18:43:02 +0100

    Completed no14 in Array2

---
+++ codingJS/Array2/no14.cs
@@ -0,0 +1,32 @@
+namespace cSharp_withMrMike.no14;
+
+// Given an array of ints, return true if it contains no 1's or it contains no 4's.
+
+// Examples
+
+// no14([1, 2, 3]) → true
+// no14([1, 2, 3, 4]) → false
+// no14([2, 3, 4]) → true
+
+public class No14
+{
+    public static bool num14(int[] no)
+    {
+        bool isResult = true;
+       
+        for(int i=0; i<no.Length; i++)
+        {
+             if(no[i] != 1 && no[i] != 4)
+            {
+                isResult = true;
+            }
+            else
+            {
+                isResult = false;
+            }
+        }
+        
+
+        return isResult;
+    }
+}
