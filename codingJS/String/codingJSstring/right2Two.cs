29c62a928135891bf271ba7cb9894ed63baa8c73
ENE-DEDE PRECIOUS DEINMA <enededeprecious1@gmail.com>
2026-07-14 13:23:08 +0100

    Completed task conCat in String1

---
+++ codingJS/String/codingJSstring/right2Two.cs
@@ -0,0 +1,29 @@
+namespace cSharp_withMrMike.String;
+
+// Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+//  The string length will be at least 2.
+
+// Examples
+
+// right2('Hello') → loHel
+// right2('java') → vaja
+// right2('Hi') → Hi
+
+public class RightTwo
+{
+    public static string rightTwo(string word)
+    {
+        string rev = "";
+
+        if(word.Length<=2)
+        {
+            return word;
+        }
+        else
+        {
+            for(int i=word.Length-2; i>= )
+        }
+
+        
+    }
+}
