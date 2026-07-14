cb310faef68d65b6128e271fbbb967e92d54a025
ENE-DEDE PRECIOUS DEINMA <enededeprecious1@gmail.com>
2026-07-14 12:27:05 +0100

    Completed task helloName in String1

--- codingJS/String/codingJSstring/helloName.cs
+++ codingJS/String/codingJSstring/helloName.cs
@@ -1,20 +1,19 @@
-using System.Transactions;
+// using System.Transactions;
 
-namespace cSharp_withMrMike.String;
+// namespace cSharp_withMrMike.String;
 
-// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+// // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 
-// Examples
+// // Examples
 
-// helloName('Bob') → Hello Bob!
-// helloName('Alice') → Hello Alice!
-// helloName('X') → Hello X!
+// // helloName('Bob') → Hello Bob!
+// // helloName('Alice') → Hello Alice!
+// // helloName('X') → Hello X!
 
-public class HelloName
-{
-    public static void myName(string name)
-    {
-        Console.WriteLine($"Hello {name}!");
-
-    }
-}
+// public class HelloName
+// {
+//     public static void myName(string name)
+//     {
+//         Console.WriteLine($"Hello {name}!");
+//     }
+// }
