39c9b3b5b398ae84aa7b11fbe915cfd65cac6340
ENE-DEDE PRECIOUS DEINMA <enededeprecious1@gmail.com>
2026-07-15 21:11:12 +0100

    Completed task firstHalf in String1

---
+++ codingJS/String/codingJSstring/firstHalf.cs
@@ -0,0 +1,23 @@
+namespace cSharp_withMrMike.String;
+
+// Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+// Examples
+
+// firstHalf('WooHoo') → WooHoo
+// firstHalf('HelloThere') → HelloThere
+// firstHalf('abcdefg') → abcdefg
+
+public class FirstHalf
+{
+    public static string firstH(string word)
+    {
+        string newString = ""; 
+        for(int i =0; i<word.Length/2; i++)
+        {
+            newString += word[i];
+        }
+
+        return newString;
+    }
+}
