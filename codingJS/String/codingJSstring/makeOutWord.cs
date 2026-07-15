82ccbe9d2c0eab15505668193ff4a43e41d40cb5
ENE-DEDE PRECIOUS DEINMA <enededeprecious1@gmail.com>
2026-07-15 11:16:20 +0100

    Completed task makeOutWord in String1

---
+++ codingJS/String/codingJSstring/makeOutWord.cs
@@ -0,0 +1,23 @@
+namespace cSharp_withMrMike.String;
+
+// Given an "out" string length 4, such as "<<>>", and a word, return a 
+// new string where the word is in the middle of the out string, e.g. "<<word>>". 
+// Note: use str.substring(i, j) to extract the String starting at index i 
+// and going up to but not including index j.
+
+// Examples
+
+// makeOutWord('<<>>', 'Yay') → <>
+// makeOutWord('<<>>', 'WooHoo') → <>
+// makeOutWord('[[]]', 'word') → [[word]]
+
+public class makeOutWord
+{
+    public static string makeOutW(string word1, string word2)
+    {
+        // string contain = " ";
+       string newword = word1.Insert(2,word2);
+
+        return newword;
+    }
+}
